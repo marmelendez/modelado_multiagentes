6a7d977ca78e9ea087a979ba605ef6e9cbc5f413
Maribel Melendez <lizbethmelendez@live.com>
2021-11-30 20:06:29 -0600

    Comment and order index file

--- ModelCar.js
+++ ModelCar.js
@@ -169,8 +169,8 @@ export default class Car extends THREE.LOD {
     this.low = new CarLow(x, z, color);
     this.high = new CarHigh(x, z, color);
 
-    this.addLevel(this.high, 50);
-    this.addLevel(this.low, 75);
+    this.addLevel(this.high, 100);
+    this.addLevel(this.low, 200);
   }
 
   setVisible(value) {
